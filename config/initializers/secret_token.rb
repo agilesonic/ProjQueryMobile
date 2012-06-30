d34da55dadd47de1fc2be789dac5b9bbc04e813f
agilesonic <agilesonic@gmail.com>
2012-06-30 08:03:38 -0400

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ProjQueryMobile::Application.config.secret_token = '86fc166bf83d6e2c89d2f4abdd84fcc3d013038bec4f5e5c5271e23f5a2662869a5b2010a6e5da56969bdccae0a107a3510501538ad9ef1eaef2c288512792a3'
